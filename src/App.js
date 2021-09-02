64dfe554738908cffc82479a5e1e5f5320c1b733
farhanzaman123 <farhanzaman2@gmail.com>
2021-09-02 12:59:19 -0400

    adding everything

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import axios from 'axios';
+class App extends Component {
+  componentDidMount() {
+    }
+    clickFunction = () =>{
+      const api = 'https://dypmcttoh7.execute-api.us-east-1.amazonaws.com/test';
+      const data = { "name" : "Yasin" };
+      axios
+        .post(api, data)
+        .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
+  render() {
+    return (
+      <div>
+      <div>Medium Tutorial</div>
+      <button onClick={this.clickFunction}>Click</button>
+      </div>
+    );
+  }
 }
-
 export default App;
